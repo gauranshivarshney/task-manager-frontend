02bc05af728839fa07d91170beaddfa8d826bf8f
TechAce <93094408+gauranshivarshney@users.noreply.github.com>
2025-05-09 16:23:34 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/task-manager-frontend",
+  base: "/task-manager-frontend/",
 
   server: {
     port: 3000,
