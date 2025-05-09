1bf19fbe0bc43e328828234a20040f0c2a5584e3
TechAce <93094408+gauranshivarshney@users.noreply.github.com>
2025-05-09 16:26:02 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/task-manager-frontend/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
